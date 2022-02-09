75b334f069af9703b89cd90177b5cf56e23aa74e
hanako.kirameki <hanako.kirameki@gmail.com>
2022-02-09 09:50:11 +0000

    CRUD tasks

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-use App\Tasks; //追加
+use App\Task; //追加
 
 class TasksController extends Controller
 {
@@ -33,7 +33,11 @@ class TasksController extends Controller
      */
     public function create()
     {
-        //
+        $task = new Task;
+
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
     }
 
     /**
@@ -44,7 +48,11 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
     }
 
     /**
@@ -55,7 +63,10 @@ class TasksController extends Controller
      */
     public function show($id)
     {
-        //
+        $task = Task::find($id);
+        return view('tasks.show',[
+            'task' => $task,
+            ]);
     }
 
     /**
@@ -66,7 +77,11 @@ class TasksController extends Controller
      */
     public function edit($id)
     {
-        //
+        $task = Task::find($id);
+        
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
     }
 
     /**
@@ -78,7 +93,12 @@ class TasksController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $task = Task::find($id);
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+        
     }
 
     /**
@@ -89,6 +109,9 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::find($id);
+        $task->delete();
+        
+        return redirect('/');
     }
 }
