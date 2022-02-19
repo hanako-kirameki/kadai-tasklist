3512a0edb585c2bed10c4dc95ae989f3dbd76bcd
hanako.kirameki <hanako.kirameki@gmail.com>
2022-02-19 00:43:00 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -3,16 +3,29 @@
 @section('content')
 
     <h1>タスク新規作成ページ</h1>
+
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class='col-lg-offset-3 col-sm-6'>
+            {!! Form::model($task,['route' => 'tasks.store']) !!}
     
-    {!! Form::model($task,['route' => 'tasks.store']) !!}
+            <div class="form-group">
+                {!! Form::label('status','ステータス:') !!}
+                {!! Form::text('status',null, ['class' => 'form-control']) !!}
+            </div>
     
-        {!! Form::label('status','ステータス:') !!}
-        {!! Form::text('status') !!}
+            <div class="form-group">
+                {!! Form::label('content','タスク:')!!}
+                {!! Form::text('content',null, ['class' => 'form-control'])!!}
+            </div>
+        </div>
+        </div>
+        </div>
+    </div>
     
-        {!! Form::label('content','タスク:')!!}
-        {!! Form::text('content')!!}
         
-        {!! Form::submit('投稿') !!}
+            {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
         
     {!! Form::close() !!}
     
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,13 +4,22 @@
 
     <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-sm-offset-2 col-sm-8">
+        <div class='col-lg-offset-3 col-sm-6'>
+    
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
     
+       <div class="form-group">
         {!! Form::label('status','ステータス:') !!}
-        {!! Form::text('status') !!}
+        {!! Form::text('status',null,['class'=> 'form-control']) !!}
+    　</div>
 
+       <div class="form-group">
         {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
+        {!! Form::text('content',null,['class' => 'form-control']) !!}
+       </div>
 
         {!! Form::submit('更新') !!}
 
